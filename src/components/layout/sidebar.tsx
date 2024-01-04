e8ff2eb4689226947ab20baf6278d125b9615055
QuocAnh189 <anhquoc18092003@gmail.com>
2024-01-04 14:45:26 +0700

    update

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -3,7 +3,7 @@ import { navItems } from "src/constants/data";
 import { cn } from "src/lib/utils";
 
 // import { Playlist } from "../data/playlists";
-
+import { useTheme } from "next-themes";
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
   // playlists: Playlist[];
   side: boolean;
@@ -11,6 +11,8 @@ interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export default function Sidebar({ className, side, onChange }: SidebarProps) {
+  const { theme } = useTheme();
+  console.log(theme);
   return (
     <div className={cn("py-16 border", className)}>
       <div className="space-y-4 py-4">
@@ -22,10 +24,22 @@ export default function Sidebar({ className, side, onChange }: SidebarProps) {
             <DashboardNav items={navItems} />
           </div>
           <button className="relative" onClick={onChange}>
-            <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
-            <span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900 dark:bg-transparent">
+            <div
+              className={`absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded ${
+                theme === "light" ? "bg-black" : "bg-white"
+              }`}
+            ></div>
+            <div
+              className={`fold-bold relative inline-block h-full w-full rounded border-2 ${
+                theme === "light" ? "border-black" : "border-white"
+              } ${
+                theme === "light" ? "bg-white" : "bg-black"
+              } px-3 py-1 text-base font-bold ${
+                theme === "light" ? "text-black" : "text-black"
+              } transition duration-100 hover:bg-yellow-400 hover:text-gray-900 dark:bg-transparent`}
+            >
               {side ? "LTR" : "RTL"}
-            </span>
+            </div>
           </button>
         </div>
       </div>
